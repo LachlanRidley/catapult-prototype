cad4b88b4e13148ba4e37c8aacadaa4052b4d77e
Lachlan Ridley <lachie.ridley@gmail.com>
2024-10-20 03:02:31 +0200

    Update build tools to use setuptools

---
+++ README.md
@@ -0,0 +1,13 @@
+# catapult-prototype
+
+## build tools
+
+This repo includes some simple python build tools.
+
+To install the build tools:
+
+```bash
+python -m pip install .
+```
+
+To modify the build tools, make changes to tools.py and then reinstall.
--- tools.py
+++ tools.py
@@ -2,24 +2,27 @@ import subprocess
 import click
 import shutil
 
+
 @click.group()
 def cli():
     pass
 
-@cli.command()
-def build():
-    build_internal()
 
 @cli.command()
 def run():
-    build_internal()
-    subprocess.run(["PlaydateSimulator", "game.pdx"])
+    build()
+    run_internal()
 
-def build_internal():
+
+def build():
     if shutil.which("pdc") is None:
         raise Exception("pdc is not on path; install PlayDate SDK")
 
     subprocess.run(["pdc", "source", "game.pdx"])
 
-if __name__ == '__main__':
-    cli()
+
+def run_internal():
+    if shutil.which("PlaydateSimulator") is None:
+        raise Exception("PlaydateSimulator is not on path; install PlayDate SDK")
+
+    subprocess.Popen(["PlaydateSimulator", "game.pdx"])
