76ff833015d19366809644f33fff8b5c8d6f55ce
Lachlan Ridley <lachie.ridley@gmail.com>
2024-10-20 03:02:30 +0200

    Move slime into class

--- source/main.lua
+++ source/main.lua
@@ -28,22 +28,83 @@ pd.start()
 -- globals
 local SCREEN_WIDTH <const> = 400
 local SCREEN_HEIGHT <const> = 240
+local FRAME_RATE <const> = 30
+local DT <const> = 1 / FRAME_RATE
 
-local slimeX = SCREEN_WIDTH / 2
-local slimeY = SCREEN_HEIGHT / 2
+local MAX_VELOCITY = 280
+local GRAVITY_CONSTANT = 1680
+local GRAVITY_STEP = pd.geometry.vector2D.new(0, GRAVITY_CONSTANT * DT)
 
-local VELOCITY <const> = 10
+local HOP_VELOCITY = 890
 
-local angle = 0
-local dx = 0
-local dy = 0
+local FRICTION_CONSTANT <const> = 0.1
+
+---@class Slime: _Sprite
+Slime = class('Slime').extends(gfx.sprite) or Slime
+
+---@type Slime
+local slime = nil
+
+function Slime:init(x, y)
+	Slime.super.init(self)
+	self.velocity = pd.geometry.vector2D.new(0, 0)
+	self:moveTo(x, y)
+
+	self.angle = 0
+
+	self:setSize(40, 40)
+end
+
+function Slime:update()
+	if not pd:isCrankDocked() then
+		self.angle = pd.getCrankPosition()
+	end
+
+	-- gravity
+	self.velocity = self.velocity + GRAVITY_STEP
+
+	-- friction
+	self.velocity = self.velocity + self.velocity:scaledBy(-FRICTION_CONSTANT)
+
+	local velocityStep = self.velocity * DT
+	self:moveTo(self.x + velocityStep.dx, self.y + velocityStep.dy)
+
+	if self.y >= SCREEN_HEIGHT then
+		self.velocity.dy = 0
+		self.velocity.dx = 0
+		self:moveTo(self.x, SCREEN_HEIGHT)
+	end
+
+	if self.x <= 0 then
+		self:moveTo(0, self.y)
+		self.velocity.dx = -self.velocity.dx
+	elseif self.x >= SCREEN_WIDTH then
+		self:moveTo(SCREEN_WIDTH, self.y)
+		self.velocity.dx = -self.velocity.dx
+	end
+
+	if pd.buttonJustPressed(pd.kButtonB) then
+		self.velocity.dx = HOP_VELOCITY * math.sin(math.rad(self.angle))
+		self.velocity.dy = HOP_VELOCITY * -math.cos(math.rad(self.angle))
+	end
+end
+
+function Slime:draw()
+	local pointerOffsetX = 20 * math.sin(math.rad(self.angle))
+	local pointerOffsetY = 20 * -math.cos(math.rad(self.angle))
+
+	gfx.drawLine(20, 20, 20 + pointerOffsetX, 20 + pointerOffsetY)
+	gfx.fillCircleAtPoint(20, 20, 5)
+end
 
 function Setup()
 	-- set the game up
-	pd.display.setRefreshRate(50)
+	pd.display.setRefreshRate(FRAME_RATE)
 
 	-- set up game menu
 	local menu = playdate.getSystemMenu()
+	slime = Slime(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+	slime:add()
 
 	-- menu:addMenuItem("Restart game", function()
 	-- 	RestartGame()
@@ -51,57 +112,11 @@ function Setup()
 end
 
 function pd.update()
-	gfx.clear()
-
-	if not pd:isCrankDocked() then
-		angle = pd.getCrankPosition()
-	end
-
-	slimeX += dx
-	slimeY += dy
-
-	-- TODO friction needs to be proportional to direction
-	if dx < 0 then
-		dx = math.min(0, dx + 0.2)
-	elseif dx > 0 then
-		dx = math.max(0, dx - 0.2)
-	end
-
-	if slimeY >= SCREEN_HEIGHT then
-		dy = 0
-		slimeY = SCREEN_HEIGHT
-	else
-		dy = math.min(dy + 0.2, 10)
-	end
-
-	if slimeY >= SCREEN_HEIGHT then
-		slimeY = SCREEN_HEIGHT
-	end
-
-	if slimeX <= 0 then
-		slimeX = 0
-		dx = -dx
-	elseif slimeX >= SCREEN_WIDTH then
-		slimeX = SCREEN_WIDTH
-		dx = -dx
-	end
-
-	gfx.drawText("angle " .. angle, 10, 10)
-	gfx.drawText("dx " .. dx, 10, 20)
-
-	if pd.buttonJustPressed(pd.kButtonB) then
-		dx = VELOCITY * math.sin(math.rad(angle))
-		dy = VELOCITY * -math.cos(math.rad(angle))
-	end
-
-	local pointerOffsetX = 20 * math.sin(math.rad(angle))
-	local pointerOffsetY = 20 * -math.cos(math.rad(angle))
-
-	gfx.drawLine(slimeX, slimeY, slimeX + pointerOffsetX, slimeY + pointerOffsetY)
-	gfx.fillCircleAtPoint(slimeX, slimeY, 5)
-
-	gfx.sprite.redrawBackground()
 	gfx.sprite.update()
+
+	gfx.drawText("angle " .. slime.angle, 10, 10)
+	gfx.drawText("dx " .. slime.velocity.dx, 10, 30)
+
 	timer.updateTimers()
 end
 
