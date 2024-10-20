da89b2a6a261d8c24a2d14446495c63e402f70ae
Lachlan Ridley <lachie.ridley@gmail.com>
2024-10-20 03:02:32 +0200

    Little refactoring as a treat

--- source/scenes/game.lua
+++ source/scenes/game.lua
@@ -4,25 +4,12 @@ local pd <const> = playdate
 local gfx <const> = pd.graphics
 local timer <const> = pd.timer
 
----@class LevelInstance
----@field slime Slime
----@field walls Wall[]
----@field spikes Spikes[]
----@field goal Goal
-
----Clears everything related to a given level instance. Should be run before starting a level or changing scene
----@param level LevelInstance
-local function UnloadLevel(level)
-    gfx.sprite.removeSprite(level.slime)
-    gfx.sprite.removeSprites(level.walls)
-    gfx.sprite.removeSprites(level.spikes or {})
-    gfx.sprite.removeSprite(level.goal)
-end
-
-
 ---@class Game
 ---@field level Level
----@field levelInstance LevelInstance | nil
+---@field slime Slime | nil
+---@field walls Wall[] | nil
+---@field spikes Spikes[] | nil
+---@field goal Goal | nil
 Game = class("Game").extends() or Game
 
 ---Runs a level
@@ -34,15 +21,15 @@ function Game:init(level)
 end
 
 function Game:update()
-    if self.levelInstance.goal:getBoundsRect():containsPoint(self.levelInstance.slime:getPosition()) then
-        UnloadLevel(self.levelInstance)
+    if self.goal:getBoundsRect():containsPoint(self.slime:getPosition()) then
+        self:ClearLevel()
         CompleteLevel(self.level)
 
         return
     end
 
-    for spike in All(self.levelInstance.spikes or {}) do
-        if spike:getBoundsRect():containsPoint(self.levelInstance.slime:getPosition()) then
+    for spike in All(self.spikes or {}) do
+        if spike:getBoundsRect():containsPoint(self.slime:getPosition()) then
             self:StartLevel()
         end
     end
@@ -50,18 +37,27 @@ function Game:update()
     gfx.sprite.update()
 
     if DEBUG then
-        gfx.drawText("angle " .. self.levelInstance.slime.angle, 10, 10)
-        gfx.drawText("dx " .. self.levelInstance.slime.velocity.dx, 10, 30)
-        gfx.drawText("stuck " .. tostring(self.levelInstance.slime.stuck), 10, 50)
+        gfx.drawText("angle " .. self.slime.angle, 10, 10)
+        gfx.drawText("dx " .. self.slime.velocity.dx, 10, 30)
+        gfx.drawText("stuck " .. tostring(self.slime.stuck), 10, 50)
     end
 
     timer.updateTimers()
 end
 
 function Game:StartLevel()
-    if self.levelInstance then
-        UnloadLevel(self.levelInstance)
-    end
+    self:ClearLevel()
 
-    self.levelInstance = self.level.load()
+    local levelContent = self.level.load()
+    self.slime = levelContent.slime
+    self.walls = levelContent.walls
+    self.spikes = levelContent.spikes
+    self.goal = levelContent.goal
+end
+
+function Game:ClearLevel()
+    if self.slime then gfx.sprite.removeSprite(self.slime) end
+    if self.walls then gfx.sprite.removeSprites(self.walls) end
+    if self.spikes then gfx.sprite.removeSprites(self.spikes) end
+    if self.goal then gfx.sprite.removeSprite(self.goal) end
 end
