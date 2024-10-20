c309cadd6548e59ca756d7c605ab11798ad6c8ab
Lachlan Ridley <lachie.ridley@gmail.com>
2024-10-20 03:02:32 +0200

    Add LevelSwitcher UI control

--- source/scenes/menu.lua
+++ source/scenes/menu.lua
@@ -2,9 +2,30 @@ local pd <const> = playdate
 local gfx <const> = pd.graphics
 
 ---@class Menu
+---@field switcher LevelSwitcher
 Menu = class("Menu").extends() or Menu
 
 function Menu:init()
+    self.switcher = LevelSwitcher(20, SCREEN_HEIGHT / 2)
+end
+
+function Menu:update()
+    if pd.buttonJustPressed(pd.kButtonDown) then
+        self.switcher:next()
+    elseif pd.buttonJustPressed(pd.kButtonUp) then
+        self.switcher:previous()
+    elseif pd.buttonIsPressed(pd.kButtonA) or pd.buttonIsPressed(pd.kButtonB) then
+        self.switcher:remove()
+        LoadLevel(self.switcher.selectedLevelIndex)
+    end
+
+    gfx.sprite.update()
+end
+
+---@class LevelSwitcher
+LevelSwitcher = class("LevelSwitcher").extends() or LevelSwitcher
+
+function LevelSwitcher:init(x, y)
     self.selectedLevelIndex = 1
     self.levelTexts = {}
 
@@ -16,21 +37,28 @@ function Menu:init()
 
     self.menuText = gfx.sprite.new(self.levelTexts[self.selectedLevelIndex])
     self.menuText:setCenter(0, 0)
-    self.menuText:moveTo(10, 10)
+    self.menuText:moveTo(x, y)
     self.menuText:add()
 end
 
-function Menu:update()
-    if pd.buttonJustPressed(pd.kButtonDown) then
-        self.selectedLevelIndex = self.selectedLevelIndex + 1
-        if self.selectedLevelIndex > #LEVELS then
-            self.selectedLevelIndex = 1
-        end
-        self.menuText:setImage(self.levelTexts[self.selectedLevelIndex])
-    elseif pd.buttonIsPressed(pd.kButtonA) then
-        self.menuText:remove()
-        LoadLevel(self.selectedLevelIndex)
+function LevelSwitcher:next()
+    self.selectedLevelIndex = self.selectedLevelIndex + 1
+    if self.selectedLevelIndex > #LEVELS then
+        self.selectedLevelIndex = 1
     end
 
-    gfx.sprite.update()
+    self.menuText:setImage(self.levelTexts[self.selectedLevelIndex])
+end
+
+function LevelSwitcher:previous()
+    self.selectedLevelIndex = self.selectedLevelIndex - 1
+    if self.selectedLevelIndex < 1 then
+        self.selectedLevelIndex = #LEVELS
+    end
+
+    self.menuText:setImage(self.levelTexts[self.selectedLevelIndex])
+end
+
+function LevelSwitcher:remove()
+    self.menuText:remove()
 end
